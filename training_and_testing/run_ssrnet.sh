779517633d9aeea5a44ff72d98c1e91915cfb6a2
ranishreee <48724306+ranishreee@users.noreply.github.com>
2020-10-03 12:29:40 +0530

    Update run_ssrnet.sh

--- training_and_testing/run_ssrnet.sh
+++ training_and_testing/run_ssrnet.sh
@@ -5,8 +5,8 @@ do
     for j in `seq 1 $max`
     do
         echo "$j"
-        KERAS_BACKEND=tensorflow python SSRNET_train.py --input ../data/imdb_db.npz --db imdb --netType1 $i --netType2 $j
-        KERAS_BACKEND=tensorflow python SSRNET_train.py --input ../data/wiki_db.npz --db wiki --netType1 $i --netType2 $j --batch_size 50
+        KERAS_BACKEND=tensorflow python SSRNET_train.py --input ../data/imdb.npz --db imdb --nb_epochs 10 --netType1 $i --netType2 $j
+        KERAS_BACKEND=tensorflow python SSRNET_train.py --input ../data/wiki.npz --db wiki --nb_epochs 10 --netType1 $i --netType2 $j --batch_size 50
         KERAS_BACKEND=tensorflow python SSRNET_train.py --input ../data/morph2_db_align.npz --db morph --netType1 $i --netType2 $j --batch_size 50
     done
 done
